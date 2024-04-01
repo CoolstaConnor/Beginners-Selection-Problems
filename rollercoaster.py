c144f70b50d9ca66a6697705297b1c7950e6147a
CoolstaConnor <160691687+CoolstaConnor@users.noreply.github.com>
2024-04-01 10:41:07 +0000

    Rollercoaster Code Created

---
+++ rollercoaster.py
@@ -0,0 +1,6 @@
+height = int(input("How tall are you in centimetres?"))
+minimum = 120
+if height >= minimum:
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry. You can not ride the rollercoaster.")
